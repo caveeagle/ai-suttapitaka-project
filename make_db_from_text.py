4ecc5e380ea115310461d2126f4fb0841419b3ff
Cave <a@a.com>
2026-01-14 20:27:06 +0100

    table chapters_info added

---
+++ make_db_from_text.py
@@ -0,0 +1,123 @@
+"""
+Script to put data from text files into DB, in table 'segments'
+"""
+import os
+import json
+import sqlite3
+
+##################################################
+
+base_path = './sutta-data'
+
+db = 'sutta-pitaka.sqlite'
+
+##################################################
+
+print(f'Begin to work...')
+
+raw_data = []
+
+counter = 1
+
+for filename in sorted(os.listdir(base_path)):
+    if not filename.lower().endswith('.json'):
+        continue
+
+    file_path = os.path.join(base_path, filename)
+
+    with open(file_path, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    file_id = os.path.splitext(filename)[0]
+    
+    file_id = file_id.split('_translation', 1)[0]
+    
+    for line_id, text in data.items():
+         
+        raw_data.append((file_id, line_id, text, counter))
+        counter += 1
+
+print(f'Read {len(raw_data)} strings from files')  # 245733
+
+##################################################
+
+line_ids = (row[1] for row in raw_data)   # генератор, не копирует данные
+s = set()
+
+for x in line_ids:
+    if x in s:
+        raise AssertionError(f'Duplicate segment_id: {x}')
+    s.add(x)
+
+##################################################
+
+db_data = []
+
+skipped = 0
+
+prev_is_empty = False
+
+for (file_id, segment_id, text, row_id) in raw_data:
+
+    # book — до первой точки или до двоеточия, что раньше
+
+    #an9.29:1.4
+    #pli-tv-kd11:31.1.185
+    
+    s = segment_id
+    book = s[:min(x if x != -1 else len(s) for x in (s.find('.'), s.find(':')))]
+
+    ####
+    
+    # sutta — до двоеточия
+    sutta = segment_id.split(':', 1)[0]
+    
+    content = text.strip()
+    
+    if content.isdigit(): # like empty strings
+        content = ''
+        
+    if content.startswith('Numbered Discourses'):
+        skipped += 1
+        continue
+    
+    is_empty = (content == '')
+
+    if is_empty and prev_empty:
+        skipped += 1
+        continue   # skip consecutive empty lines
+    
+    prev_empty = is_empty        
+    
+    db_data.append((row_id, segment_id, book, file_id, sutta, content))
+
+print(f'Skipped {skipped} strings')
+
+db_data.sort(key=lambda x: x[0])
+
+##################################################
+
+with sqlite3.connect(db) as conn:
+    
+    cursor = conn.cursor()
+    
+    cursor.execute('DELETE FROM segments')
+    
+    cursor.executemany('''
+        INSERT INTO segments
+        (row_id, segment_id, book, chapter, sutta, content)
+        VALUES (?, ?, ?, ?, ?, ?)
+    ''', db_data)
+    
+    inserted = cursor.rowcount
+    
+    conn.commit()
+
+print(f'Insert {inserted} rows into database')
+
+assert ( len(raw_data) == skipped+inserted ), 'Something wrong...'
+
+##################################################
+
+print(f'Job finished')    
+    
---
+++ make_table_chapters.sql
@@ -0,0 +1,69 @@
+CREATE INDEX IF NOT EXISTS idx_segments_chapter_row
+ON segments (chapter, row_id);
+
+##################################################
+
+SELECT
+    segments_outer.chapter,
+    COUNT(*) AS rows_count,
+    SUM(LENGTH(segments_outer.content)) AS total_chars,
+
+    MIN(segments_outer.row_id) AS start_row_id,
+    (
+        SELECT segments.segment_id
+        FROM segments
+        WHERE segments.chapter = segments_outer.chapter
+        ORDER BY segments.row_id ASC
+        LIMIT 1
+    ) AS start_segment_id,
+
+    MAX(segments_outer.row_id) AS end_row_id,
+    (
+        SELECT segments.segment_id
+        FROM segments
+        WHERE segments.chapter = segments_outer.chapter
+        ORDER BY segments.row_id DESC
+        LIMIT 1
+    ) AS end_segment_id
+
+FROM segments AS segments_outer
+GROUP BY segments_outer.chapter;
+
+##################################################
+
+INSERT INTO chapters_info (
+    chapter,
+    rows_count,
+    total_chars,
+    start_row_id,
+    start_segment_id,
+    end_row_id,
+    end_segment_id
+)
+SELECT
+    segments_outer.chapter,
+    COUNT(*) AS rows_count,
+    SUM(LENGTH(segments_outer.content)) AS total_chars,
+
+    MIN(segments_outer.row_id) AS start_row_id,
+    (
+        SELECT segments.segment_id
+        FROM segments
+        WHERE segments.chapter = segments_outer.chapter
+        ORDER BY segments.row_id ASC
+        LIMIT 1
+    ) AS start_segment_id,
+
+    MAX(segments_outer.row_id) AS end_row_id,
+    (
+        SELECT segments.segment_id
+        FROM segments
+        WHERE segments.chapter = segments_outer.chapter
+        ORDER BY segments.row_id DESC
+        LIMIT 1
+    ) AS end_segment_id
+FROM segments AS segments_outer
+GROUP BY segments_outer.chapter;
+
+##################################################
+
