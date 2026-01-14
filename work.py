fb408e4a68e7fa556cdd64662f0bed40d5e312ce
Cave <a@a.com>
2026-01-14 22:31:23 +0100

    Add validation questions

--- work.py
+++ work.py
@@ -9,8 +9,11 @@ db = 'sutta-pitaka.sqlite'
 
 MAX_TOKENS_IN_CHAPTER = 800
 
-OPTIMAL_TOKENS = 6
-00
+OPTIMAL_TOKENS = 400  # 1 token = 4 chars
+
+OVERLAP_TOKENS = 70  # ~10-20% of chunk
+
+OVERLAP_STRINGS = OVERLAP_TOKENS // 17.5
 
 #########################################################
 #########################################################
@@ -21,6 +24,8 @@ with sqlite3.connect(db) as conn:
     cursor.execute('SELECT * FROM chapters_info ORDER BY id')
     chapters_info = cursor.fetchall()    
 
+#########################################################
+#########################################################
 #########################################################
 
 chunks = []
@@ -38,32 +43,58 @@ for (
 
     if( total_chars//4 < MAX_TOKENS_IN_CHAPTER ): #  small chapter - one chunk
         
+        chunk = {}
+        
         chunk['chapter'] = chapter
         chunk['start_row_id'] = start_row_id
         chunk['end_row_id'] = end_row_id
         chunk['start_seg_id'] = start_segment_id
         chunk['end_seg_id'] = start_segment_id
         
-        chunks.add(chunk)
+        chunks.append(chunk)
     
     else: # BIG chapter    
 
-        num_of_splits = ( (total_chars/4) // OPTIMAL_TOKENS ) + 1
+        num_of_splits = ( (total_chars//4) // OPTIMAL_TOKENS ) + 1
 
         str_per_split = math.ceil( (end_row_id - start_row_id + 1) / num_of_splits )
         
         for N in range( num_of_splits ):
             
-            chunk['
+            chunk_start_shift = ( N * str_per_split) - OVERLAP_STRINGS
+            
+            if chunk_start_shift < 0:
+                chunk_start_shift = 0
+            
+            chunk_end_shift = ( (N+1) * str_per_split) - 1
+            
+            chunk_end_shift += OVERLAP_STRINGS
+            
+            if( chunk_end_shift > (end_row_id-start_row_id) ):
+                
+                chunk_end_shift = end_row_id-start_row_id
+            
+            chunk = {}
+            
+            chunk['start_row_id'] = start_row_id + chunk_start_shift
+            
+            chunk['end_row_id'] = start_row_id + chunk_end_shift
+            
+            chunk['chapter'] = chapter
+            
+            chunks.append(chunk)
 
+print(f'Make {len(chunks)} chunks')
 
+#########################################################
+#########################################################
+#########################################################
 
 
 
 #########################################################
 #########################################################
 #########################################################
-
 
 print('Job finished')    
     
