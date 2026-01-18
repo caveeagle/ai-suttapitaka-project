f961551f2ee5966f2774fbd0fe72a47130306d2a
Cave <a@a.com>
2026-01-18 20:38:57 +0100

    ToDo added

---
+++ ToDo.md
@@ -0,0 +1,7 @@
+# Todo list
+
+1. Finish validate script
+2. Make clear script to answer a question (as a function)
+3. Add GUI interface for this script
+4. Write readmy
+5. Start Flask project
--- validate_model.py
+++ validate_model.py
@@ -11,27 +11,39 @@ from google.genai.errors import ClientError
 #########################################################
 #########################################################
 
-DEBUG = 1
+####  MODE OF SCRIPT:  ###
+
+#MODE = 'VALIDATION'
+
+MODE = 'TESTING'
+
+'''
+
+TESTING - give the last question, and just show the answer
+
+VALIDATION - test all questions, and compare answers with true answ.
+
+'''
+
+
+#####  MODEL PARAMETERS:   #####  
 
 TOP_K = 5
 
 TEMPERATURE = 0.2
 
-###MODEL = 'gemini-2.0-flash-lite'
 MODEL = 'gemini-2.5-pro'
 
+#####  QUESTION FOR VALIDATION:   #####
 
-QUESTION = (
-    'According to the Buddha, which sensory experiences '
-    'most strongly occupy a man’s mind?'
-)
+QUESTIONS_MAP = {  # ORDER IS IMPORTANT !
+    1: 'According to the Buddha, which sensory experiences most strongly occupy a man’s mind?',
+    2: 'Can a health monk eat after noon?',
+    3: 'In which year was the Buddha born?',
+    4: 'Can a woman become an Arahant?',
+}
 
-QUESTION = 'Can a health monk eat after noon?'
-
-QUESTION = 'In which year was the Buddha born?'
-
-QUESTION = 'Can a woman become an Arahant?'
-
+QUESTIONS = [QUESTIONS_MAP[i] for i in sorted(QUESTIONS_MAP)]
 
 #########################################################
 #########################################################
@@ -67,112 +79,129 @@ Sources:
 #########################################################
 #########################################################
 
-print(f'Begin to work...')
+print(f'Begin to work...\n')
 
-API_KEY = secret_config.API_KEY
-
-if not API_KEY or not API_KEY.strip():
-    raise ValueError('API_KEY is empty or not set')
-
-client = genai.Client(api_key=API_KEY)
-
-index, chunk_ids = build_index()
-
-resp = client.models.embed_content(
-    model='models/text-embedding-004',
-    contents=[QUESTION],
-)
-
-time.sleep(2)
-
-print(f'Question embedded!')
-
-query_vec = np.array(resp.embeddings[0].values, dtype=np.float32)
-
-top_chunk_ids, scores = search(index, chunk_ids, query_vec, k=TOP_K)
-
-assert len(top_chunk_ids) == TOP_K 
-
-if(DEBUG):
-    print(f'Find top chunks ids: {top_chunk_ids}')
-
-#########################################################
-#########################################################
-
-db = 'sutta-pitaka.sqlite'
-
-context_lines = []
-
-placeholders = ','.join('?' for _ in top_chunk_ids)
-
-query = f'''
-SELECT *
-FROM chunks
-WHERE id IN ({placeholders})
-ORDER BY id
-'''
-
-with sqlite3.connect(db) as conn:
-    conn.row_factory = sqlite3.Row
-    cursor = conn.cursor()
-
-    cursor.execute(query, top_chunk_ids)
-    rows = cursor.fetchall()
+for QUESTION in QUESTIONS:
     
-content_blocks = []
-
-for row in rows:
-    content_blocks.append(
-        f"[CHAPTER {row['chapter']}]\n{row['content']}"
+    if MODE == 'TESTING':
+        
+         QUESTION = QUESTIONS[len(QUESTIONS)-1] #  the last
+    
+    API_KEY = secret_config.API_KEY
+    
+    if not API_KEY or not API_KEY.strip():
+        raise ValueError('API_KEY is empty or not set')
+    
+    client = genai.Client(api_key=API_KEY)
+    
+    index, chunk_ids = build_index()
+    
+    resp = client.models.embed_content(
+        model='models/text-embedding-004',
+        contents=[QUESTION],
     )
-
-SOURCES = '\n\n'.join(content_blocks)
-
-assert len(content_blocks) == TOP_K
-
-print(f'Make content blocks, ready for request:')
-
-#########################################################
-#########################################################
-
-PROMPT = PROMPT_TEMPLATE.format(
-    question=QUESTION,
-    sources=SOURCES
-)
-
-try:
     
-    response = client.models.generate_content(
-        model=MODEL,
-        contents=[PROMPT],
-        config={
-            'temperature': TEMPERATURE
-        }        
+    time.sleep(1)
+    
+    if MODE == 'TESTING':
+        print(f'Question embedded!')
+    
+    query_vec = np.array(resp.embeddings[0].values, dtype=np.float32)
+    
+    top_chunk_ids, scores = search(index, chunk_ids, query_vec, k=TOP_K)
+    
+    assert len(top_chunk_ids) == TOP_K 
+    
+    if MODE == 'TESTING':
+        print(f'Find top chunks ids: {top_chunk_ids}')
+    
+    #########################################################
+    #########################################################
+    
+    db = 'sutta-pitaka.sqlite'
+    
+    context_lines = []
+    
+    placeholders = ','.join('?' for _ in top_chunk_ids)
+    
+    query = f'''
+    SELECT *
+    FROM chunks
+    WHERE id IN ({placeholders})
+    ORDER BY id
+    '''
+    
+    with sqlite3.connect(db) as conn:
+        conn.row_factory = sqlite3.Row
+        cursor = conn.cursor()
+    
+        cursor.execute(query, top_chunk_ids)
+        rows = cursor.fetchall()
+        
+    content_blocks = []
+    
+    for row in rows:
+        content_blocks.append(
+            f"[CHAPTER {row['chapter']}]\n{row['content']}"
+        )
+    
+    SOURCES = '\n\n'.join(content_blocks)
+    
+    assert len(content_blocks) == TOP_K
+    
+    if MODE == 'TESTING':
+        print(f'Make content blocks, ready for request:')
+    
+    #########################################################
+    #########################################################
+    
+    PROMPT = PROMPT_TEMPLATE.format(
+        question=QUESTION,
+        sources=SOURCES
     )
-
-except ClientError as e:
-    msg = str(e)
-
-    if "429" in msg or "RESOURCE_EXHAUSTED" in msg:
-        print(f'ERROR 429 - LIMITS EXCEEDED!')
-        # просто выходим, ничего не выводим
-        pass
-    else:
-        raise
     
-print(f'Request finished.\n\n')
+    try:
+        
+        response = client.models.generate_content(
+            model=MODEL,
+            contents=[PROMPT],
+            config={
+                'temperature': TEMPERATURE
+            }        
+        )
+        
+        time.sleep(2)
+        
+    except ClientError as e:
+        msg = str(e)
+    
+        if "429" in msg or "RESOURCE_EXHAUSTED" in msg:
+            print(f'ERROR 429 - LIMITS EXCEEDED!')
+            # просто выходим, ничего не выводим
+            pass
+        else:
+            raise
+        
+    if MODE == 'TESTING':
+        print(f'Request finished.\n\n')
+        
+    #########################################################
+    #########################################################
+    
+    ### OUTPUT:
+    
+    if MODE == 'TESTING':
+        print('\nQUESTION:')
+        print(QUESTION)
+        print('\nANSWER:')
+        print(response.text)
+    
+    if MODE == 'TESTING':
+        break  # only last question
+    
     
 #########################################################
 #########################################################
 
-### OUTPUT:
+print(f'\nJob finished')
 
-print('\nQUESTION:')
-print(QUESTION)
-print('\nANSWER:')
-print(response.text)
-
-print(f'\n\nJob finished')
-
-#########################################################
-#########################################################
